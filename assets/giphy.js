8c4865f9029d0b664c93697f774fc209a706b255
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-03-07 21:02:39 -0500

    added bg image, styling, defaulted gifs to still

--- assets/giphy.js
+++ assets/giphy.js
@@ -3,7 +3,7 @@ $("button").on("click", function() {
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
   hedgehog + " hedgehog" + "&api_key=SMTb3tSlY0U2YO3pw0CTrzbXep49Q8EX&limit=10";
 
-  console.log(queryURL);
+ // console.log(queryURL);
 
   $.ajax({
     url: queryURL,
@@ -11,21 +11,41 @@ $("button").on("click", function() {
   })
   .then(function(response) {
     var results = response.data;
-    console.log(results);
+   // console.log(results);
     for (var i = 0; i < results.length; i++) {
-      var gifDiv = $("<div class='item'>");
+      var gifDiv = $("<div class='col-md-4'>");
 
       var rating = results[i].rating;
 
       var p = $("<p>").text("Rating: " + rating);
 
-      var hedgehogGif = $("<img>");
-      hedgehogGif.attr("src", results[i].images.fixed_height.url);
+      var hedgehogGifAnimate = $("<img>");
+      hedgehogGifAnimate.attr("src", results[i].images.fixed_width.url).addClass("giphy-image");
+
+      var hedgehogGifStill = $("<img>");
+      hedgehogGifStill.attr("src", results[i].images.fixed_width_still.url).addClass("giphy-image");
 
       gifDiv.append(p);
-      gifDiv.append(hedgehogGif);
+      gifDiv.append(hedgehogGifStill);
 
       $("#hog-giphies").prepend(gifDiv);
-    }
+    
+
+    $("img").on("click", function(results) {
+    //  console.log(results[i].images.fixed_width.url);
+      var image = $(this).attr("src");
+      if (image === hedgehogGifAnimate) {
+        $(this).replaceWith(hedgehogGifStill);
+     //   $(this).attr("data-state", "animate");
+      }
+
+      else if (image === hedgehogGifStill) {
+        $(this).replaceWith(hedgehogGifAnimate);
+     //   $(this).attr("data-state", "still");
+      }
+    }); 
+}
+  //  hedgehogGif.attr("src", results[i].images.fixed_height_still.url);
   });
+
 });
