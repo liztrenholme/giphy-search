d538dc67cc0e2263f1267123db2114a9ddccffd4
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-03-07 22:47:07 -0500

    adding buttons works, but need to fix query after buttons are added

--- assets/giphy.js
+++ assets/giphy.js
@@ -1,7 +1,9 @@
-$("button").on("click", function() {
-  var hedgehog = $(this).attr("hedgehog");
+var hedgehogs = ["Happy", "Cute", "Surprised"];
+
+$(document).on("click", ".hoggybuttons", function() {
+  var hog = $(this).attr("hog");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  hedgehog + " hedgehog" + "&api_key=SMTb3tSlY0U2YO3pw0CTrzbXep49Q8EX&limit=10";
+  hog + " hedgehog" + "&api_key=SMTb3tSlY0U2YO3pw0CTrzbXep49Q8EX&limit=10";
 
  // console.log(queryURL);
 
@@ -31,21 +33,58 @@ $("button").on("click", function() {
       $("#hog-giphies").prepend(gifDiv);
     
 
-    $("img").on("click", function(results) {
-    //  console.log(results[i].images.fixed_width.url);
-      var image = $(this).attr("src");
-      if (image === hedgehogGifAnimate) {
-        $(this).replaceWith(hedgehogGifStill);
-     //   $(this).attr("data-state", "animate");
-      }
+     
+    }
 
-      else if (image === hedgehogGifStill) {
-        $(this).replaceWith(hedgehogGifAnimate);
-     //   $(this).attr("data-state", "still");
-      }
-    }); 
-}
-  //  hedgehogGif.attr("src", results[i].images.fixed_height_still.url);
   });
 
 });
+
+$("img").on("click", function(results) {
+    //  console.log(results[i].images.fixed_width.url);
+    //  var image = $(this).attr("src");
+      if ($("img").attr("src") === hedgehogGifAnimate) {
+        $("img").attr("src", hedgehogGifStill);
+     //   $(this).attr("data-state", "animate");
+      }
+
+      else if ($("img").attr("src") === hedgehogGifStill) {
+        $("img").attr("src", hedgehogGifAnimate);
+     //   $(this).attr("data-state", "still");
+      }
+    });
+
+function makeButtons() {
+        $("#hog-buttons").empty();
+
+        for (var i = 0; i < hedgehogs.length; i++) {
+
+          // Then dynamicaly generating buttons for each movie in the array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var newbutton = $("<button>");
+          // Adding a class of movie-btn to our button
+          newbutton.addClass("hoggybuttons");
+          // Adding a data-attribute
+          newbutton.attr("data-name", hedgehogs[i]);
+          // Providing the initial button text
+          newbutton.text(hedgehogs[i]);
+          // Adding the button to the buttons-view div
+          $("#hog-buttons").append(newbutton);
+        }
+      }
+
+$("#add-hog").on("click", function(event) {
+        event.preventDefault();
+
+        // This line grabs the input from the textbox
+        var hog = $("#search-hogs").val().trim();
+
+        // The movie from the textbox is then added to our array
+        hedgehogs.push(hog);
+
+        // Calling renderButtons which handles the processing of our hog array
+        makeButtons();
+
+      });
+
+//$(document).on("click", ".hoggybuttons", displayMovieInfo);
